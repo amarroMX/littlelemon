9adc2ca5c655345f5e46da3220b35f4867d01806
amarroMx <abdiwali.maxamed.hersi@gmail.com>
2023-06-23 18:21:27 +0200

    booking api completed

--- restaurant/models.py
+++ restaurant/models.py
@@ -5,6 +5,8 @@ class Menu(models.Model):
     Title = models.CharField(max_length=255)
     Price = models.DecimalField(max_digits=10, decimal_places=2)
     Inventory = models.IntegerField()
+    Created_at = models.DateTimeField(auto_now_add=True)
+    Last_modified = models.DateTimeField(auto_now=True)
     
     def  __str__(self):
         return self.Title
@@ -14,7 +16,9 @@ class Menu(models.Model):
 class Booking(models.Model):
     Name = models.CharField(max_length=255)
     NoOfGuest = models.IntegerField()
-    BookingDate = models.DateTimeField(auto_created=True)
+    BookingDate = models.DateTimeField()
+    Created_at = models.DateTimeField(auto_now_add=True)
+    Last_modified = models.DateTimeField(auto_now=True)
     
     def __str__(self) -> str:
         return self.Name
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import render
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView 
+from rest_framework.viewsets import ModelViewSet
 from .models import Menu, Booking
 from .serializers import MenuSerializer, BookingSerializer
 
 
-class MenuItemView(ListCreateAPIView):
+class MenuItemsView(ListCreateAPIView):
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
    
 class SingleMenuItemView(RetrieveUpdateDestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+    
+class BookingViewSet(ModelViewSet):
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
 
