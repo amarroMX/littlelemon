161ff26857fecff83a8399fa2f6727fbe5d32987
amarroMX <abdiwal.maxamed.hersi@gmail.com>
2023-06-28 13:58:51 +0200

    menu model tests all most commpleted , two failing tests

--- restaurant/models.py
+++ restaurant/models.py
@@ -2,24 +2,24 @@ from django.db import models
 
 
 class Menu(models.Model):
-    Title = models.CharField(max_length=255)
-    Price = models.DecimalField(max_digits=10, decimal_places=2)
-    Inventory = models.IntegerField()
-    Created_at = models.DateTimeField(auto_now_add=True)
-    Last_modified = models.DateTimeField(auto_now=True)
-    
-    def  __str__(self):
-        return self.Title
-    
-    
-    
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return f"{self.title} - {self.price}"
+
+
 class Booking(models.Model):
-    Name = models.CharField(max_length=255)
-    NoOfGuest = models.IntegerField()
-    BookingDate = models.DateTimeField()
-    Created_at = models.DateTimeField(auto_now_add=True)
-    Last_modified = models.DateTimeField(auto_now=True)
-    
+    name = models.CharField(max_length=255)
+    no_of_guests = models.IntegerField()
+    booking_date = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+
+
     def __str__(self) -> str:
-        return self.Name
-    
+        return f"{self.name} booked for {self.no_of_guests} persons on {self.booking_date}"
